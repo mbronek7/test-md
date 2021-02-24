0f3c037a10bd29f598421253304f4a90cd26d675
mbronek7 <mbronek7@gmail.com>
2021-02-24 22:51:54 +0100

    fix

--- app/assets/javascripts/active_admin/simplemde_editor_input.js
+++ app/assets/javascripts/active_admin/simplemde_editor_input.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $('.simplemde-editor').each(function () {
-        options = $.extend({}, options, $(this).data('options'));
         new EasyMDE( {
 			element: $(this).get(0)
 		});
